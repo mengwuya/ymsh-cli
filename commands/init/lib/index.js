34f70dce7856eecf24b9bed6379f972249636313
zhanghl <987202055@qq.com>
2022-09-13 23:45:29 +0800

    feat: 新增项目名称及版本号的校验

--- commands/init/lib/index.js
+++ commands/init/lib/index.js
@@ -1,24 +1,158 @@
-'use strict';
+"use strict";
 
 const Command = require("@ymsh-cli/command");
 const log = require("@ymsh-cli/log");
+const fs = require("fs");
+const fse = require("fs-extra");
+const inquirer = require("inquirer");
+const semver = require("semver");
+
+const TYPE_PROJECT = "project";
+const TYPE_COMPONENT = "component";
 
 class InitCommand extends Command {
-    init() {
-        this.projectName = this._argv[0] || '';
-        this.force = !!this._cmd.force;
-        log.verbose('projectName', this.projectName);
-        log.verbose('force', this.force);
-    }
+  init() {
+    this.projectName = this._argv[0] || "";
+    this.force = !!this._cmd.force;
+    log.verbose("projectName", this.projectName);
+    log.verbose("force", this.force);
+  }
 
-    exec() {
-        console.log('init的业务');
+  isDirEmpty(localPath) {
+    let fileList = fs.readdirSync(localPath);
+    fileList = fileList.filter((file) => {
+      return !file.startsWith(".") && ["node_modules"].indexOf(file) < 0;
+    });
+    return !fileList || fileList.length <= 0;
+  }
+
+  async prepare() {
+    const localPath = process.cwd();
+    if (!this.isDirEmpty(localPath)) {
+      let ifContinue = false;
+      if (!this.force) {
+        // 询问是否继续创建
+        ifContinue = (
+          await inquirer.prompt({
+            type: "confirm",
+            name: "ifContinue",
+            default: false,
+            message: "当前文件夹不为空，是否继续创建项目？",
+          })
+        ).ifContinue;
+        if (!ifContinue) {
+          return;
+        }
+      }
+      // 2. 是否启动强制更新
+      if (ifContinue || this.force) {
+        // 给用户做二次确认
+        const { confirmDelete } = await inquirer.prompt({
+          type: "confirm",
+          name: "confirmDelete",
+          default: false,
+          message: "是否确认清空当前目录下的文件？",
+        });
+        if (confirmDelete) {
+          // 清空当前目录
+          fse.emptyDirSync(localPath);
+        }
+      }
     }
+    return this.getProjectInfo();
+  }
+
+  async getProjectInfo() {
+    const projectInfo = {};
+    // 1、选择创建项目或组件
+    const { type } = await inquirer.prompt({
+      type: "list",
+      message: "请选择初始化类型",
+      default: TYPE_PROJECT,
+      name: "type",
+      choices: [
+        {
+          name: "项目",
+          value: TYPE_PROJECT,
+        },
+        {
+          name: "组件",
+          value: TYPE_COMPONENT,
+        },
+      ],
+    });
+    if (type === TYPE_PROJECT) {
+      // 2、获取项目的基本信息
+      const o = await inquirer.prompt([
+        {
+          type: "input",
+          message: "请输入项目名称",
+          name: "projectName",
+          default: "",
+          validate: function (v) {
+            const done = this.async();
+            setTimeout(function () {
+              // 输入的首字符必须为英文字符 尾字符必须为英文或数字，不能为字符 字符仅允许 "-_"
+              if (
+                !/^[a-zA-Z]+([-][a-zA-Z][a-zA-Z0-9]*|[_][a-zA-Z][a-zA-Z0-9]*|[a-zA-Z0-9])*$/.test(
+                  v
+                )
+              ) {
+                done("请输入合法的项目名称");
+                return;
+              }
+              done(null, true);
+            }, 0);
+          },
+          filter: function (v) {
+            return v;
+          },
+        },
+        {
+          type: "input",
+          name: "projectVersion",
+          message: "请输入项目版本号",
+          default: "1.0.0",
+          validate: function (v) {
+            const done = this.async();
+            setTimeout(function () {
+              if (!!!semver.valid(v)) {
+                done("请输入合法的版本号");
+                return;
+              }
+              done(null, true);
+            }, 0);
+          },
+          filter: function (v) {
+            if (!!semver.valid(v)) {
+              return semver.valid(v);
+            } else {
+              return v;
+            }
+          },
+        },
+      ]);
+    } else {
+      // 组件
+    }
+    return projectInfo;
+  }
+
+  async exec() {
+    try {
+      // 1、准备阶段
+      await this.prepare();
+      // 2、下载模版
+      // 3、安装模板
+    } catch (e) {
+      log.error(e.message);
+    }
+  }
 }
 
 function init(argv) {
-    // console.log('init', projectName, comObj.force, process.env.CLI_TARGET_PATH);
-    return new InitCommand(argv);
+  // console.log('init', projectName, comObj.force, process.env.CLI_TARGET_PATH);
+  return new InitCommand(argv);
 }
 
 module.exports = init;
